8f90acd9d8be7da85630153b81187fe8cf9dc0bc
Samya abhiraj bharti <109699745+saumyabhartii@users.noreply.github.com>
2022-07-21 23:47:33 +0530

    higher order function

--- hoc/higherOrderComponent.js
+++ hoc/higherOrderComponent.js
@@ -1,1 +1,7 @@
 //add higher order component
+function User(){
+    let authenticate = "some value";
+    function SuperUser(){
+        console.log(authenticate);  //can be accessed here;
+    }
+}
